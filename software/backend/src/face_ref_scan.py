53e8e61c0f8a449f60cefa1ce3c55b580776e98b
Victoria Sun <vpsun@uci.edu>
2025-10-30 00:39:29 -0700

    Created image_capture()

--- software/backend/src/face_ref_scan.py
+++ software/backend/src/face_ref_scan.py
@@ -1,2 +1,21 @@
-# finish by pm5
-# picking skin regions from the sample picture
+import cv2
+
+def image_capture():
+
+    cap = cv2.VideoCapture(0)
+
+    if not cap.isOpened():
+        print("Error: Could not open camera.")
+    else:
+        ret, frame = cap.read()
+
+    if ret:
+        cv2.imshow("Captured Image", frame)
+        cv2.waitKey(0) # Wait indefinitely until a key is pressed
+        cv2.destroyAllWindows()
+    else:
+        print("Error: Could not read frame.")
+
+    cap.release()
+
+    return ret, frame
