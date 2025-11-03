8a1cc561763a5ae591792d6273839c9ebd3ea5d7
dalenas <dalenavrinsmith@gmail.com>
2025-11-03 08:53:15 -0800

    Defined linear RGB to XYZ color space conversion.

--- software/backend/src/rgb_to_lab.py
+++ software/backend/src/rgb_to_lab.py
@@ -13,11 +13,15 @@ def lighting_correction(captured_reference, captured_skin):
     stored_reference_pinv = np.lingalg.pinv(const.REFERENCE_LINEAR_RGB)
     lighting_matrix = stored_reference_pinv @ captured_reference
     lighting_matrix_inv = np.lingalg.inv(lighting_matrix)
-    actual_skin = captured_skin @ lighting_matrix_inv
-    return actual_skin
+    lighting_corrected_skin = captured_skin @ lighting_matrix_inv
+    return lighting_corrected_skin
 
+# xyz_codes = RGB_TO_XYZ_MATRIX @ lighting_corrected_skin
 # [3 x 1] = [3 x 3] x [3 x 1]
 def linear_to_xyz(linear_codes):
-    
+    linear_codes_T = np.transpose(linear_codes)
+    xyz_codes_T = const.RGB_TO_XYZ_MATRIX @ linear_codes_T
+    xyz_codes = np.transpose(xyz_codes_T)
+    return xyz_codes
 
 def xyz_to_lab():
