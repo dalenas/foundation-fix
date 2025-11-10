beaea2ceae2f547a83da898bc32f5f711ea6a99c
Harmeet Singh <harmees2@uci.edu>
2025-11-10 09:39:13 -0800

    feat(color_algorithm.py): implement lab_avg_to_rgb and image_to_hex function stubs

--- software/backend/src/color_algorithm.py
+++ software/backend/src/color_algorithm.py
@@ -2,3 +2,9 @@ from face_ref_scan import define_skin
 from rgb_to_lab import gamma_to_linear, lighting_correction, linear_to_xyz, xyz_to_lab, average_lab
 import numpy as np
 from PIL import Image
+
+def lab_avg_to_rgb(lab_avg):
+    return
+
+def image_to_hex(image_path):
+    return
